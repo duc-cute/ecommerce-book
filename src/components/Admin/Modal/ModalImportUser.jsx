b7fc5a812d919fa6fd379486eb1f85692fcb095e
Duc nguyen <ducnguyen150120035555@gmail.com>
2023-07-19 01:14:28 +0700

    #5 read file xlsx

--- src/components/Admin/Modal/ModalImportUser.jsx
+++ src/components/Admin/Modal/ModalImportUser.jsx
@@ -5,27 +5,17 @@ import Dragger from "antd/es/upload/Dragger";
 
 import { InboxOutlined } from "@ant-design/icons";
 import { message, Upload } from "antd";
-const columns = [
-  {
-    title: "Tên hiển thị",
-    dataIndex: "fullName",
-    key: "fullName",
-  },
-  {
-    title: "Email",
-    dataIndex: "email",
-    key: "email",
-  },
-  {
-    title: "Số điện thoại",
-    dataIndex: "phone",
-    key: "phone",
-  },
-];
+import * as XLSX from "xlsx";
+import { useState } from "react";
 
 const ModalImportUser = ({ open, setOpen }) => {
+  const [dataImport, setDataImport] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isDisable, setIsDisable] = useState(true);
+
   const handleOk = () => {
     setOpen(false);
+    setDataImport([]);
   };
 
   const dummyRequest = ({ file, onSuccess }) => {
@@ -45,10 +35,11 @@ const ModalImportUser = ({ open, setOpen }) => {
     onChange(info) {
       const { status } = info.file;
       if (status !== "uploading") {
-        console.log(info.file, info.fileList);
+        console.log("file", info.file);
       }
       if (status === "done") {
         message.success(`${info.file.name} file uploaded successfully.`);
+        handleUpload(info.file.originFileObj);
       } else if (status === "error") {
         message.error(`${info.file.name} file upload failed.`);
       }
@@ -57,17 +48,44 @@ const ModalImportUser = ({ open, setOpen }) => {
       console.log("Dropped files", e.dataTransfer.files);
     },
   };
+
+  const handleUpload = (f) => {
+    setIsLoading(true);
+    let reader = new FileReader();
+    reader.onload = function (e) {
+      let data = new Uint8Array(e.target.result);
+      let readedData = XLSX.read(data, { type: "array" });
+      const ws = readedData.Sheets["Sheet1"];
+
+      const dataParse = XLSX.utils.sheet_to_json(ws);
+      // const dataParse = XLSX.utils.sheet_to_json(ws, {
+      //   headers: ["fullName","email","phone"], Fomat data
+      //   range:1
+      // });
+      if (dataParse) {
+        setDataImport(dataParse);
+        setIsDisable(false);
+        console.log("dataP", dataParse);
+      }
+    };
+    reader.readAsArrayBuffer(f);
+    setIsLoading(false);
+  };
   return (
     <Modal
       width={800}
       title="Import data user"
       open={open}
       onOk={handleOk}
-      onCancel={() => setOpen(false)}
+      onCancel={() => {
+        setOpen(false);
+        setDataImport([]);
+      }}
       okText="Import Data"
       okButtonProps={{
-        disabled: true,
+        disabled: isDisable,
       }}
+      style={{ top: 40 }}
     >
       <Dragger {...props}>
         <p className="ant-upload-drag-icon">
@@ -81,7 +99,27 @@ const ModalImportUser = ({ open, setOpen }) => {
         </p>
       </Dragger>
       <p style={{ margin: "48px 0 12px 6px" }}>Dữ liệu upload:</p>
-      <Table dataSource={[]} columns={columns} />;
+      <Table
+        sticky
+        rowKey={"Email"}
+        dataSource={dataImport}
+        style={{ maxHeight: "280px", overflow: "auto" }}
+        loading={isLoading}
+        columns={[
+          {
+            title: "Tên hiển thị",
+            dataIndex: "Tên hiển thị",
+          },
+          {
+            title: "Email",
+            dataIndex: "Email",
+          },
+          {
+            title: "Số điện thoại",
+            dataIndex: "Số điện thoại",
+          },
+        ]}
+      />
     </Modal>
   );
 };
